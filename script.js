b38843492092f9607217c3116257cd7bef78f6ba
Pedro Henrique <pehdfms@gmail.com>
2021-09-25 10:48:02 -0300

    fix javascript support for chrome

---
+++ script.js
@@ -0,0 +1,26 @@
+function startTime() {
+    const currentDate = new Date();
+
+    let hours   = padTime(currentDate.getHours());
+    let minutes = padTime(currentDate.getMinutes());
+    let seconds = padTime(currentDate.getSeconds());
+
+    let year  = currentDate.getYear() + 1900;
+    let month = padTime(currentDate.getMonth());
+    let day   = padTime(currentDate.getDay());
+
+    date = year + "/" + month + "/" + day;
+    time = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("time").innerHTML = date + "<br>" + time;
+
+    setTimeout(startTime, 1000);
+}
+
+function padTime(timeUnit) {
+    if (timeUnit < 10) {
+        return "0" + timeUnit;
+    }
+
+    return timeUnit;
+}
